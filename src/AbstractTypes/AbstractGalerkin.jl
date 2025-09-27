30a036b6a22fd9cb7e5b49e786beffe70e05b929
DanielBoigk <dn.boigk@gmail.com>
2025-09-27 15:26:47 +0200

    add AbstractMethods

--- src/AbstractTypes/AbstractGalerkin.jl
+++ src/AbstractTypes/AbstractGalerkin.jl
@@ -3,8 +3,9 @@ module HilbertSpaces
 export AbstractHilbertSpace,AbstractGalerkinSolver, AbstractBoundaryPair
 
 
-abstract type AbstractHilbertSpace end # This is supposed to hold all information about the space:
+abstract type AbstractHilbertSpace end # This is supposed to hold all information about the space
 abstract type AbstractGalerkinSolver end # This is supposed to hold all the information that the solver needs
+abstract type AbstractAdjointSolver end # This is supposed to be all needed to get a gradient δσ for the update of σ 
 abstract type AbstractBoundaryPair end # This is supposed to halod all the information for a voltage-current boundary pair.
 
 
