7f82097134670d32d5c5ff4597c5417d491d7100
DanielBoigk <dn.boigk@gmail.com>
2025-09-27 14:23:49 +0200

    add test

---
+++ tests/runtests.jl
@@ -0,0 +1,13 @@
+using Ferrite
+using SparseArrays
+using LinearAlgebra
+using Revise
+using Interpolations
+using Statistics
+using IterativeSolvers
+using LinearMaps
+
+
+# Try to run test
+println("Running Test: ")
+@assert false
